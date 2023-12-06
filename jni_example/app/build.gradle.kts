9fda3369e103576fdb01618c4eb9d752a2b4d53c
Daiwei Lu <daiweilu123@gmail.com>
2023-12-06 12:17:12 -0800

    Fix compile issue

--- jni_example/app/build.gradle.kts
+++ jni_example/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         externalNativeBuild {
             cmake {
                 cppFlags += "-std=c++17"
@@ -25,30 +26,33 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
         }
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    // 1.9.0 is the latest we can use to compile against SDK 33
+    // noinspection GradleDependency
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- jni_example/build.gradle.kts
+++ jni_example/build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // `apply false` means that the plugin is declared at the top level but not immediately applied
+    // to all modules in the project.
     id("com.android.application") version "8.1.3" apply false
 }
